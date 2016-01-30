16c166f5999dfc95c81f647cd7980f9709379303
processor1 <alimark96@yahoo.com>
2016-01-30 09:03:13 +0000

    Update NetworkInterfaceCard.java

--- NetworkInterfaceCard.java
+++ NetworkInterfaceCard.java
@@ -1,1 +1,16 @@
+/*Java Network Class Library */
+/*Processor-Hacks */
 
+
+public class NetworkInterfaceCard{
+  
+  
+  /*main method */
+  
+  public static  void main(java.lang.String[] argv){
+    /*body of the main method */
+  }
+  
+  
+  /*end of the class */
+}
